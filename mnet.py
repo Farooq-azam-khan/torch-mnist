47d41fc9d51a8e2c3edb50b3594f925e6c1e0ca1
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-01-24 13:51:40 -0500

    figured out issue with softmax that prevents it from acheiving higher accuracy - need to know why this is the case

--- mnet.py
+++ mnet.py
@@ -11,15 +11,14 @@ class MNet(nn.Module):
     def __init__(self): 
         super().__init__()
 
-        self.l1 = nn.Linear(image_height*image_width, 800)
-        self.l2 = nn.Linear(800, 10)
-        self.output = nn.Softmax(dim=1)
-
+        self.l1 = nn.Linear(image_height*image_width, 128)
+        self.l2 = nn.Linear(128, 10)
+        self.opt = nn.Softmax(dim=1) # not working as expected (lower testing accuracy)
     def forward(self, x): 
         x = self.l1(x)
         x = F.relu(x)
         x = self.l2(x)
-        return self.output(x)
+        return x
 
 
 def accuracy(y_pred, labels):
@@ -31,14 +30,14 @@ train_dataset_labels = torch.tensor(np.load('./data/train-labels-idx1-ubyte.npy'
 print('Read Training Dataset and Labels')
 model = MNet()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0)
-criterion = nn.NLLLoss()
+criterion = nn.CrossEntropyLoss()
 batch_size = 32
 epochs = 3
 from tqdm import trange 
 for epoch in range(epochs):
     print(f'Epoch: {epoch+1}')
     running_loss = 0.0 
-    for i in trange(train_dataset_images.shape[0]-batch_size):
+    for i in (t := trange(train_dataset_images.shape[0]-batch_size)):
         optimizer.zero_grad() 
         imgs = train_dataset_images[i:i+batch_size, :, :].reshape((-1, image_width*image_height))
         imgs_pred = model(imgs)
@@ -46,10 +45,12 @@ for epoch in range(epochs):
         loss = criterion(imgs_pred, imgs_labels)
         loss.backward()
         optimizer.step() 
+        t.set_description(f'Loss: {loss.item():.2f}')
 
         running_loss += loss.item() 
+    running_loss = running_loss / len(range(train_dataset_images.shape[0]-batch_size))
     y_pred = model(train_ds).argmax(dim=1) 
-    print(f'Finished Epoch {epoch+1} {running_loss/batch_size}, Training Accuracy: {accuracy(y_pred, train_dataset_labels)}')
+    print(f'Finished Epoch {epoch+1} {running_loss}, Training Accuracy: {accuracy(y_pred, train_dataset_labels)}')
 print('Finished Training')
 
 y_pred = model(train_ds).argmax(dim=1) 
